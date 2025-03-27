1d23dd25743a6c352f2faf066f21b7ec0aa8a41b
njongefred9 <frednjonge9@gmail.com>
2025-03-27 21:17:41 +0300

    Controller tests for sign up flow

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -1,13 +1,44 @@
 require "test_helper"
 
 class UsersControllerTest < ActionDispatch::IntegrationTest
-  test "should get new" do
-    get users_new_url
-    assert_response :success
+  test "redirects to feed after successful sign up" do
+    get sign_up_path
+    assert_response :ok
+
+    assert_difference [ "User.count", "Organization.count" ], 1 do
+      post sign_up_path, params: {
+        user: {
+          name: "Test User",
+          email: "test@example.com",
+          password: "password",
+          password_confirmation: "password"
+        }
+      }
+    end
+
+    assert_redirected_to root_path
+    follow_redirect!
+    assert_select ".notification.is-success",
+      text: I18n.t("users.create.welcome", name: "Test User")
   end
 
-  test "should get create" do
-    get users_create_url
-    assert_response :success
+  test "render errors if input data is invalid" do
+    get sign_up_path
+    assert_response :ok
+
+    assert_no_difference [ "User.count", "Organization.count" ] do
+      post sign_up_path, params: {
+        user: {
+          name: "John",
+          email: "john@example.com",
+          password: "pass",
+          password_confirmation: "pass"
+        }
+      }
+    end
+
+    assert_response :unprocessable_entity
+    assert_select "p.is-danger",
+      text: I18n.t("activerecord.errors.models.user.attributes.password.too_short")
   end
 end
