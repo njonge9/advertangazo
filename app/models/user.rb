9f037d2b748c332d3f8c211063811529463d8ec2
njongefred9 <frednjonge9@gmail.com>
2025-03-27 18:09:54 +0300

    Validations and testing the models

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,16 @@
 class User < ApplicationRecord
+  validates :name, presence: true
+  validates :email, format: { with: URI::MailTo::EMAIL_REGEXP }, uniqueness: { case_sensitive: false }
+
+  has_many :memberships, dependent: :destroy
+  has_many :organizations, through: :memberships
+
+  before_validation :strip_extra_spaces
+
+  private
+
+  def strip_extra_spaces
+    self.name = self.name&.strip
+    self.email = self.email&.strip
+  end
 end
--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,7 +1,41 @@
 require "test_helper"
 
 class UserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "requires a name" do
+    @user = User.new(name: "", email: "john@example.com")
+    assert_not @user.valid?
+
+    @user.name = "John"
+    assert @user.valid?
+  end
+
+  test "requires a valid email" do
+    @user = User.new(name: "John", email: "")
+    assert_not @user.valid?
+
+    @user.email = "invalid"
+    assert_not @user.valid?
+
+    @user.email = "john@example.com"
+    assert @user.valid?
+  end
+
+  test "requires a unique email" do
+    @existing_user = User.create(
+      name: "John", email: "jd@example.com"
+    )
+    assert @existing_user.persisted?
+
+    @user = User.new(name: "Jon", email: "jd@example.com")
+    assert_not @user.valid?
+  end
+
+  test "name and email is stripped of spaces before saving" do
+    @user = User.create(
+      name: " John ", email: " John@example.com ",
+    )
+
+    assert_equal "John", @user.name
+    assert_equal "John@example.com", @user.email
+  end
 end
