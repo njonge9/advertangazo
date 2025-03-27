731ce6ab89974d810c65ace53bccb60ebdcbc79f
njongefred9 <frednjonge9@gmail.com>
2025-03-27 20:51:24 +0300

    fix the error

--- config/initializers/form_errors.rb
+++ config/initializers/form_errors.rb
@@ -1,15 +1,15 @@
-ActionView::Base.field_error_proc = -> (html_tag, instance) {
+ActionView::Base.field_error_proc = ->(html_tag, instance) {
   unless html_tag =~ /^<label/
     html = Nokogiri::HTML::DocumentFragment.parse(html_tag)
-    html.children.add_class('is-danger')
+    html.children.each { |child| child.add_class("is-danger") }
 
     error_message_markup = <<~HTML
-      <p class="help is-danger>
+      <p class="help is-danger">
         #{sanitize(instance.error_message.to_sentence)}
       </p>
     HTML
 
-    "#{html.to_s}#{error_message_markup}".html_safe
+    "#{html}#{error_message_markup}".html_safe
   else
     html_tag
   end
